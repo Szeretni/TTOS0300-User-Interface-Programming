65427e9280c3a4ddd32e598212cc7c991705f1dc
Hannu Oksman <hannu@muaddibs.net>
2018-03-08 16:30:56 +0200

    WIP

---
+++ Examples/01_MyAudioVideoPlayer/MyAudioVideoPlayer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace MyAudioVideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //content to textbox
+            PathTextBox.Text = path;
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.ShowDialog();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
