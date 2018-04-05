698a2651f1a36884b3c40c118c43902655306a8a
Hannu Oksman <hannu@muaddibs.net>
2018-04-05 18:55:48 +0300

    autotalli and birds examples

---
+++ Examples/L04_Autotalli/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.IT;
+
+namespace Autotalli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //kuvat
+        private const string polku = @"C:\Users\l2912\Source\Repos\TTOS0300-User-Interface-Programming\Examples\L04_Autotalli\";
+        List<Auto> autot = new List<Auto>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //aloituskuva
+            NaytaKuva("autotalli.png");
+            //ladataan kaikki autot muistiin
+            autot = JAMK.IT.Autotalli.HaeAutot();
+            //täytetään combobox autojen merkeillä
+            //vaihtoehto 1 manuaalisesti
+            List<String> merkit = new List<string>();
+            merkit.Add("Audi");
+            merkit.Add("Saab");
+            merkit.Add("Volvo");
+            cmbMerkit.ItemsSource = merkit;
+            //vaihtoehto 2 linqlla datasta
+            var result = autot.Select(m => m.Merkki).Distinct();
+            cmbMerkit.ItemsSource = result;
+        }
+
+        private void NaytaKuva(string url)
+        {
+            try
+            {
+                //lisätään vakiopolku kuvatiedostoon
+                if (url != "")
+                {
+                    url = polku + url;
+                    //kuvan näyttö
+                    if (System.IO.File.Exists(url))
+                    {
+                        BitmapImage bi = new BitmapImage();
+                        bi.BeginInit();
+                        bi.UriSource = new Uri(url);
+                        bi.EndInit();
+                        imgAuto.Stretch = Stretch.Fill;
+                        imgAuto.Source = bi;
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            dgAutot.ItemsSource = JAMK.IT.Autotalli.HaeAutot();
+        }
+
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /*
+            //olemme itse populoineet DataGridin Auto-olioilla, joten kukin
+            //DataGridin rivi eli alkio on itse asiassa Auto-olio
+            object obj = dgAutot.SelectedItem;
+            if (obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+            */
+            //TAI lyhyesti
+            Auto valittu = dgAutot.SelectedItem as Auto;
+            if (valittu != null)
+            {
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAuditontent_Click(object sender, RoutedEventArgs e)
+        {
+            //näkyviin suodattamalla autot-listasta audit. Käytetään LINQ
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+
+        private void cmbMerkit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string merkki = cmbMerkit.SelectedItem.ToString();
+            var result = autot.Where(m => m.Merkki.Contains(merkki)).ToList();
+            dgAutot.ItemsSource = result;
+            NaytaKuva("autotalli.png");
+        }
+    }
+}
---
+++ Examples/L04_Birds/BirdViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    class BirdViewModel
+    {
+        public static List<Bird> Get3TestBirds()
+        {
+            List<Bird> birds = new List<Bird>();
+            birds.Add(new Bird { Name = "Angry", ImgFile = "BirdRed.PNG" ,Value=1000});
+            birds.Add(new Bird { Name = "Nasty", ImgFile = "BirdBlack.PNG", Value = 500 });
+            birds.Add(new Bird { Name = "Curious", ImgFile = "BirdYellow.PNG", Value = 250 });
+            return birds;
+        }
+    }
+}
