71be14ab646b8f2dd07f6aa4bbf45c5f16154c48
Hannu Oksman <hannu@muaddibs.net>
2018-04-09 17:24:04 +0300

    entity framework example

---
+++ Examples/LXX_Entity_Framework/WpEF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using System.Collections.ObjectModel;
+
+namespace WpEF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            WineCellaryEntities db = new WineCellaryEntities();
+            db.customer.Load();
+            ObservableCollection<customer> localcustomers;
+            localcustomers = db.customer.Local;
+            dgCustomers.ItemsSource = localcustomers;
+        }
+    }
+}
